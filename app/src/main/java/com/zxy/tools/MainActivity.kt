c534b18032f7c85206b07310955711f307bd90ee
xingyu.zhang <xingyu.zhang@croninfo.com>
2019-09-16 19:09:09 +0800

    自定义AlertDialog完成

--- app/src/main/java/com/zxy/tools/MainActivity.kt
+++ app/src/main/java/com/zxy/tools/MainActivity.kt
@@ -2,11 +2,79 @@ package com.zxy.tools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.zxy.zxytools.AlertDialogUtils
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var alertDialogUtils: AlertDialogUtils
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun onStyle1(view: View) {
+        alertDialogUtils =
+            AlertDialogUtils
+                .build(this)
+                .setTransparency(0.4f)
+                .showStyle1(object : AlertDialogUtils.Builder.AlertDialogUtilsListener {
+                    override fun onClickDialog(view: View) {
+                        when (view.id) {
+                            R.id.dialog_msg_confirm -> {
+                                Toast.makeText(this@MainActivity, "确定", Toast.LENGTH_SHORT).show()
+                            }
+                            R.id.dialog_msg_cancel -> {
+                                Toast.makeText(this@MainActivity, "取消", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        alertDialogUtils.cancel()
+                    }
+                })
+    }
+
+    fun onStyle2(view: View) {
+        alertDialogUtils =
+            AlertDialogUtils
+                .build(this)
+                .setTransparency(0.8f)
+                .showStyle2(object : AlertDialogUtils.Builder.AlertDialogUtilsListener {
+                    override fun onClickDialog(view: View) {
+                        when (view.id) {
+                            R.id.dialog_msg_confirm -> {
+                                Toast.makeText(this@MainActivity, "确定", Toast.LENGTH_SHORT).show()
+                            }
+                            R.id.dialog_msg_cancel -> {
+                                Toast.makeText(this@MainActivity, "取消", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        alertDialogUtils.cancel()
+                    }
+                })
+    }
+
+    fun onCoustDikaog(view: View) {
+        alertDialogUtils = AlertDialogUtils.build(this)
+            .setView(R.layout.dialog_button)
+            .setCancelable(true)
+            .setTransparency(0f)
+            .setOnClick(R.id.but1, R.id.but2, R.id.but3)
+            .show(object : AlertDialogUtils.Builder.AlertDialogUtilsListener {
+                override fun onClickDialog(view: View) {
+                    when (view.id) {
+                        R.id.but1 -> {
+                            Toast.makeText(this@MainActivity, "but1", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.but2 -> {
+                            Toast.makeText(this@MainActivity, "but2", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.but3 -> {
+                            Toast.makeText(this@MainActivity, "but3", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    alertDialogUtils.cancel()
+                }
+            })
     }
 }
--- zxytools/src/main/java/com/zxy/zxytools/AlertDialogUtils.kt
+++ zxytools/src/main/java/com/zxy/zxytools/AlertDialogUtils.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.tools_zxy_dialog_msg1.view.*
+import android.view.WindowManager
+
 
 /**
  * Created by zxy on 2019/8/24-10:13
@@ -16,70 +19,138 @@ import android.widget.Toast
  */
 class AlertDialogUtils private constructor() {
     lateinit var layoutView: View                           //Dialog的布局文件
-    var cancelable: Boolean = true                          //是否可以取消  true可以
+    private var cancelable: Boolean = true                          //是否可以取消  true可以
     lateinit var dialog: AlertDialog                        // AlertDilaog
     lateinit var alertDilaogBuilder: AlertDialog.Builder    // AlertDilaog.Builder
+    lateinit var listView: MutableList<Int>
+    var transparency: Float = 0.5f                              // 透明度
 
     companion object {
-        fun build(mContext:Context): Builder {
+        fun build(mContext: Context): Builder {
             return Builder(mContext)
         }
     }
 
-    class Builder(mContext:Context) {
+    class Builder(mContext: Context) {
         var mContext = mContext
         var alertDialogUtils = AlertDialogUtils()
         /**
          * 设置布局
          * @param layoutView View
          */
-        fun setView(layoutView: View) {
-            alertDialogUtils.layoutView = layoutView
+        fun setView(layoutId: Int): Builder {
+            alertDialogUtils.layoutView = LayoutInflater.from(mContext).inflate(layoutId, null)
+            return this
         }
 
         /**
          * 设置点击事件
          * @param viewId IntArray
          */
-        fun setOnClick(vararg viewId: Int) {
-
+        fun setOnClick(vararg viewId: Int): Builder {
+            alertDialogUtils.listView = viewId.toTypedArray().toMutableList()
+            return this
         }
 
         /**
          * 是否可以取消  默认true可以
          * @param cancelable Boolean
          */
-        fun setCancelable(cancelable: Boolean) {
+        fun setCancelable(cancelable: Boolean): Builder {
             alertDialogUtils.cancelable = cancelable
+            return this
         }
 
         /**
-         * 显示弹出
+         * 设置窗口透明度，默认0.5    0为全透明  1全黑
+         * @param transparency Float
+         * @return Builder
          */
-        fun show(): AlertDialogUtils {
-            if(alertDialogUtils.layoutView==null){
-                Toast.makeText(mContext,"AlertDilaog的setView()不能为null",Toast.LENGTH_SHORT).show()
-            }else {
-                alertDialogUtils.alertDilaogBuilder = AlertDialog.Builder(mContext, R.style.MyDilog)
-                alertDialogUtils.alertDilaogBuilder.setView(alertDialogUtils.layoutView)
-                alertDialogUtils.dialog = alertDialogUtils.alertDilaogBuilder.create()
-                alertDialogUtils.dialog.setCancelable(alertDialogUtils.cancelable)
-                alertDialogUtils.dialog.show()
-            }
-
-            return alertDialogUtils
+        fun setTransparency(transparency: Float): Builder {
+            alertDialogUtils.transparency = transparency
+            return this
         }
 
-        fun showStyle1(){
-            alertDialogUtils.layoutView = LayoutInflater.from(mContext).inflate(R.layout.tools_zxy_dialog_msg,null)
-
+        /**
+         * 创建AlertDialog
+         */
+        private fun create() {
             alertDialogUtils.alertDilaogBuilder = AlertDialog.Builder(mContext, R.style.MyDilog)
             alertDialogUtils.alertDilaogBuilder.setView(alertDialogUtils.layoutView)
             alertDialogUtils.dialog = alertDialogUtils.alertDilaogBuilder.create()
             alertDialogUtils.dialog.setCancelable(alertDialogUtils.cancelable)
             alertDialogUtils.dialog.show()
+            val lp = alertDialogUtils.dialog.window!!.getAttributes()
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+            alertDialogUtils.dialog.window!!.setDimAmount(alertDialogUtils.transparency)//设置黑色遮罩层的透明度
+            alertDialogUtils.dialog.window!!.setAttributes(lp)
+        }
+
+
+        /**
+         * 显示弹出
+         */
+        fun show(alertDialogUtilsListener: AlertDialogUtilsListener): AlertDialogUtils {
+
+            if (alertDialogUtils.layoutView == null) {
+                Toast.makeText(mContext, "AlertDilaog的setView()不能为null", Toast.LENGTH_SHORT).show()
+            } else {
+                for (index in 0 until alertDialogUtils.listView.size step 1) {
+                    alertDialogUtils.layoutView.findViewById<View>(alertDialogUtils.listView[index])
+                        .setOnClickListener {
+                            alertDialogUtilsListener.onClickDialog(it)
+                        }
+                }
+                create()
+            }
+
+            return alertDialogUtils
+        }
+
+        /**
+         * 样式1
+         * @param alertDialogUtilsListener AlertDialogUtilsListener
+         * @return AlertDialogUtils
+         */
+        fun showStyle1(alertDialogUtilsListener: AlertDialogUtilsListener): AlertDialogUtils {
+            alertDialogUtils.layoutView = LayoutInflater.from(mContext).inflate(R.layout.tools_zxy_dialog_msg1, null)
+
+            create()
+
+            alertDialogUtils.layoutView.dialog_msg_confirm.setOnClickListener {
+                alertDialogUtilsListener.onClickDialog(it)
+            }
+            alertDialogUtils.layoutView.dialog_msg_cancel.setOnClickListener {
+                alertDialogUtilsListener.onClickDialog(it)
+            }
+            return alertDialogUtils
+        }
+
+        /**
+         * 样式2
+         * @param alertDialogUtilsListener AlertDialogUtilsListener
+         * @return AlertDialogUtils
+         */
+        fun showStyle2(alertDialogUtilsListener: AlertDialogUtilsListener): AlertDialogUtils {
+            alertDialogUtils.layoutView = LayoutInflater.from(mContext).inflate(R.layout.tools_zxy_dialog_msg2, null)
+
+            create()
+
+            alertDialogUtils.layoutView.dialog_msg_confirm.setOnClickListener {
+                alertDialogUtilsListener.onClickDialog(it)
+            }
+            alertDialogUtils.layoutView.dialog_msg_cancel.setOnClickListener {
+                alertDialogUtilsListener.onClickDialog(it)
+            }
+            return alertDialogUtils
+        }
+
+        interface AlertDialogUtilsListener {
+            fun onClickDialog(view: View)
         }
     }
+
 
     fun cancel() {
         if (dialog != null)
