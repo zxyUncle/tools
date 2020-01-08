ed71b1c878ba908cd289c77fff7c3ec11cd4cb5c
张行于 <659966631@qq.com>
2020-01-08 10:10:16 +0800

    修改点击事件

--- app/src/main/java/com/zxy/tools/MainActivity.kt
+++ app/src/main/java/com/zxy/tools/MainActivity.kt
@@ -3,14 +3,8 @@ package com.zxy.tools
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import com.zxy.zxytools.arouter.MyIntent
-import com.zxy.zxytools.extension.click
-import com.zxy.zxytools.extension.clickWithTrigger
 import com.zxy.zxytools.extension.clicks
-import com.zxy.zxytools.snackbar.Prompt
-import com.zxy.zxytools.snackbar.TSnackbar
-import com.zxy.zxytools.snackbar.TSnackbar.APPEAR_FROM_TOP_TO_DOWN
 import com.zxy.zxytools.snackbar.ZToast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,7 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        clicks(btn_main_toast, btn_main_toast1,btn_main) {
+
+        clicks(btn_main_toast, btn_main_toast1, btn_main) {
             onClick(it)
         }
     }
@@ -32,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             R.id.btn_main_toast1 -> {
                 ZToast.showI(this, "确定退出")
             }
-            R.id.btn_main->{
+            R.id.btn_main -> {
                 MyIntent.startActivityAnimation(this, FirstActivity::class.java)
             }
         }
